3a8389103759b344eae8133779416360ab738c8e
Austin Steward <austinsteward@aol.com>
2014-08-22 14:27:41 -0700

    Add route to show recipe page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   match('/recipes', {via: :get, to: 'recipes#index'})
   match('/recipes', {via: :post, to: 'recipes#new'})
+  match('/recipes/:id', {via: :get, to: 'recipes#show'})
 end
