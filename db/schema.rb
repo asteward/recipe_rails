6ef2867081965d3f37f66538af7b4e8dcb466321
Austin Steward <austinsteward@aol.com>
2014-08-22 15:35:32 -0700

    Create tag table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822181928) do
+ActiveRecord::Schema.define(version: 20140822223116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20140822181928) do
   create_table "recipes", force: true do |t|
     t.string "name"
     t.text   "content"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string "name"
   end
 
 end
