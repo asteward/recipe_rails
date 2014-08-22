b1e8b2393dad6e22cd8bf48ef6ca4746dbb7df2d
Austin Steward <austinsteward@aol.com>
2014-08-22 14:14:07 -0700

    Add 'new' function to RecipesController

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,9 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
     render('/recipes/index.html.erb')
   end
+
+  def new
+    @recipe = Recipe.new(params[:recipes])
+    render('/recipes/index.html.erb')
+  end
 end
