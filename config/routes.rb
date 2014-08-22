e315e8aca7e2eedf4b689c85636aa69b5da33764
Austin Steward <austinsteward@aol.com>
2014-08-22 14:39:57 -0700

    Add edit route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   match('/recipes', {via: :get, to: 'recipes#index'})
   match('/recipes', {via: :post, to: 'recipes#new'})
   match('/recipes/:id', {via: :get, to: 'recipes#show'})
+  match('/recipes/:id/edit', {via: :get, to: 'recipes#edit'})
 end
