1fd4123495fd25ca8c275b22493a02ae583fb122
Austin Steward <austinsteward@aol.com>
2014-08-22 22:25:11 -0700

    Fix recipes_tags_controller bug

--- app/controllers/concerns/recipes_tags_controller.rb
+++ app/controllers/concerns/recipes_tags_controller.rb
@@ -1,4 +1,4 @@
-class Recipes_TagsController < ApplicationController
+class RecipesTagsController < ApplicationController
   def index
     @recipes = Recipe.order(:name)
     @tags = Tag.order(:name)
