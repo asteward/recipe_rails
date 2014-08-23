8c502d7f70c03633f9a35eddacd81625a8a79791
Austin Steward <austinsteward@aol.com>
2014-08-22 21:57:30 -0700

    Add recipes_tags index function

--- app/controllers/concerns/recipes_tags_controller.rb
+++ app/controllers/concerns/recipes_tags_controller.rb
@@ -1,3 +1,7 @@
 class Recipes_TagsController < ApplicationController
-  
+  def index
+    @recipes = Recipe.order(:name)
+    @tags = Tag.order(:name)
+    render('/recipes_tags/index.html.erb')
+  end
 end
