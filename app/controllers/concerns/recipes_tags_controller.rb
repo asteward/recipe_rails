479d69189cb79af68c48e209951dc75b3d7db278
Austin Steward <austinsteward@aol.com>
2014-08-23 19:43:09 -0700

    Add recipes_tags create function

--- app/controllers/concerns/recipes_tags_controller.rb
+++ app/controllers/concerns/recipes_tags_controller.rb
@@ -4,4 +4,13 @@ class RecipesTagsController < ApplicationController
     @tags = Tag.order(:name)
     render('/recipes_tags/index.html.erb')
   end
+
+  def create
+    @recipe_tag = Recipe_Tag.new(params[:recipes_tags])
+    if @recipe_Tag.save
+      redirect_to("/recipes")
+    else
+      render('/recipes_tags/index.html.erb')
+    end
+  end
 end
