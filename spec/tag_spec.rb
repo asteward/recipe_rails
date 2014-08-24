ac5de6099ecbecc1b1f739ded5fcde39021b4253
Austin Steward <austinsteward@aol.com>
2014-08-24 14:45:29 -0700

    Test for tag uniqueness

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 describe Tag do
   it { should have_and_belong_to_many(:recipes) }
+
+  it 'should only create one tag per name' do
+    tag1 = Tag.create(name: 'Spicy')
+    tag2 = Tag.create(name: 'Spicy')
+    expect(Tag.all).to eq [tag1]
+  end
 end
